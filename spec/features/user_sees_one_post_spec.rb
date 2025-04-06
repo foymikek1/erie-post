4f1a312e2507811b28c2159b29be1e751b86b649
foymikek <foymikek@gmail.com>
2025-04-06 11:09:35 -0600

    Add comment feature to post show. Add partial view displaying comments.

--- spec/features/user_sees_one_post_spec.rb
+++ spec/features/user_sees_one_post_spec.rb
@@ -4,6 +4,8 @@ describe "user sees one post", type: :feature do
   describe "they link from visiting /posts" do
     it "display details of selected post" do
       post_1 = Post.create!(title: "Title 1", body: "Body 1")
+      comment_1 = post_1.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = post_1.comments.create(author_name: "You", body: "So much to say")
 
       visit '/posts'
       click_link post_1.title
@@ -11,6 +13,10 @@ describe "user sees one post", type: :feature do
 
      expect(page).to have_content(post_1.title)
      expect(page).to have_content(post_1.body)
+     expect(page).to have_content(comment_1.author_name)
+     expect(page).to have_content(comment_1.body)
+     expect(page).to have_content(comment_2.author_name)
+     expect(page).to have_content(comment_2.body)     
     end
   end
 end
