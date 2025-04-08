525c33b7a865d3c44506544473380d920325b2ff
foymikek <foymikek@gmail.com>
2025-04-08 08:31:53 -0600

    Add missing post relationship validation test. Test passing.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,7 @@ describe Post, type: :model do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
 
+    it {should have_many(:taggings)}
     it {should have_many(:comments)}
     it {should have_many(:tags).through(:taggings)}
   end
